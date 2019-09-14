8924d485439508d80fb1f07955417c97212deecd
MangLa <mang1998@naver.com>
2019-09-14 08:21:18 +0000

    진행막대기 1개로 감소 확인버튼 추가 확인버튼 누를시 자세한정보 나오게 추가 자세한정보 아직 미작성

--- roka/ROKA.js
+++ roka/ROKA.js
@@ -2,11 +2,12 @@ var app = new Vue({
   el: '#app',
   data: {
     percent:100 ,
-    Dday:0,
+    Dday:'',
     armytype:'육군',
     type1:true,
     type2:false,
-    type3:false
+    type3:false,
+	dayrender:false
   },
   methods: {
     typeset: function (type) {
@@ -27,66 +28,42 @@ var app = new Vue({
         this.armytype='공군'
       }
 	 this.percent+=2;
-    }
+    },
+	armyrender: function(){
+		this.dayrender=true;
+		
+bar.animate(1.0);
+	}
   }
 })
 //진행막대기
-var bar = new ProgressBar.Line(apbar, {
+var bar;
+bar = new ProgressBar.Line(apbar, {
+  trailColor: '#fff',
+  trailWidth: 1,
   strokeWidth: 4,
   easing: 'easeInOut',
   duration: 1300,
   color: '#FFFFFF',
+  text: {
+    style: {
+      // Text color.
+      // Default: same as stroke color (options.color)
+      color: '#111',
+      right: '0',
+      top: '30px',
+      padding: 0,
+      margin:'0 1 0 0',
+	  float: 'right',
+      transform: null
+    },
+    autoStyleContainer: false
+  },
   svgStyle: {width: '100%', height: '100%'},
   from: {color: '#FFFFFF'},
   to: {color: '#eb4d4d'},
   step: (state, bar) => {
     bar.path.setAttribute('stroke', state.color);
+	bar.setText(Math.round(bar.value() * 100) + ' %');
   }
 });
-
-
-
-var bar2 = new ProgressBar.Line(apbar2, {
-  strokeWidth: 4,
-  easing: 'easeInOut',
-  duration: 1800,
-  color: '#FFFFFF',
-  svgStyle: {width: '100%', height: '100%'},
-  from: {color: '#FFFFFF'},
-  to: {color: '#ffa01c'},
-  step: (state, bar) => {
-    bar.path.setAttribute('stroke', state.color);
-  }
-});
-
-var bar3 = new ProgressBar.Line(apbar3, {
-  strokeWidth: 4,
-  easing: 'easeInOut',
-  duration: 2300,
-  color: '#FFFFFF',
-  svgStyle: {width: '100%', height: '100%'},
-  from: {color: '#FFFFFF'},
-  to: {color: '#5ded58'},
-  step: (state, bar) => {
-    bar.path.setAttribute('stroke', state.color);
-  }
-});
-
-
-
-var bar4 = new ProgressBar.Line(apbar4, {
-  strokeWidth: 4,
-  easing: 'easeInOut',
-  duration: 2800,
-  color: '#FFFFFF',
-  svgStyle: {width: '100%', height: '100%'},
-  from: {color: '#FFFFFF'},
-  to: {color: '#3599db'},
-  step: (state, bar) => {
-    bar.path.setAttribute('stroke', state.color);
-  }
-});
-bar4.animate(1.0);
-bar3.animate(1.0);
-bar2.animate(1.0);
-bar.animate(1.0);
